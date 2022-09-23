95b96047da7f8c853652fe96042cea88f485931d
Pavel Durov <pav3ldurov@gmail.com>
2022-09-23 09:35:55 +0100

    chore: revert changes

---
+++ src/python/vanila.py
@@ -0,0 +1,35 @@
+from rpython.rlib.jit import JitDriver, purefunction, hint
+from rpython.jit.codewriter.policy import JitPolicy
+
+
+def jitpolicy(driver):
+    return JitPolicy()
+
+
+class C: pass
+
+
+jitdriver = JitDriver(greens=["i", "argv"], reds=["c"])
+
+def entry_point(argv):
+  c = C()
+  c.x = 0
+  c.y = 1
+  i = 0
+  while i < 1000000:
+    jitdriver.jit_merge_point(i=i, argv=argv, c=c)
+    c.x += c.y
+    i += 1
+  return 0
+  
+
+def target(*args):
+    """
+    "target" returns the entry point.
+    The translation process imports your module and looks for that name,
+    calls it, and the function object returned is where it starts the translation.
+    """
+    return entry_point, None
+ 
+if __name__ == '__main__':
+    entry_point(None)
