124cce5dd3eea5b89284142e2dca086962707246
kimchi <pavel@nate.tech>
2022-09-23 19:02:03 +0100

    fix: bench script file iteration

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -3,18 +3,31 @@
 set -e
 
 VERSION=$1
+MIN=$2
+MAX=$3
 
 if [ -z "$VERSION" ]; then
-  echo "Please provide project version to benchmark!"
+  echo "$VERSION argument ismendatory!"
+  exit 1
+fi
+
+if [ -z "$MIN" ]; then
+  echo "$MIN argument ismendatory!"
+  exit 1
+fi
+if [ -z "$MIN" ]; then
+  echo "$MAX argument ismendatory!"
   exit 1
 fi
 
 file_names=($( ls ./bin/${VERSION}/*-c ))
-files_to_bench=$file_names
+files_to_bench=""
 counter=0
-for file_name in $file_names ; do
-  
-  files_to_bench="'./${files_to_bench}' './${file_name}'"
+for file_name in ./bin/${VERSION}/*-c ; do
+  if [[ "$file_name" == *"-c"* ]]; then
+    files_to_bench="${file_name} ${files_to_bench}"
+  fi
 done
 
-hyperfine --warmup 10 ${files_to_bench} && hyperfine -m 100 -M 100 ${files_to_bench}
+hyperfine --warmup 10 ${files_to_bench}
+hyperfine -m ${MIN} -M ${MAX} ${files_to_bench}
